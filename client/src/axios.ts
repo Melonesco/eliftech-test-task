f3d90ca1ff1500c09d0463b7567d8cab13ae92bc
melonesco <maaxsh2002@gmail.com>
2023-06-03 14:38:50 +0300

    init

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
 export const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://deploy-back-brown.vercel.app/api/v1",
 });
--- server/index.js
+++ server/index.js
@@ -23,11 +23,15 @@ mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Mongodb connected");
-    server.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
+    if (!server.listening) {
+      server.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+      });
+    }
   })
   .catch((err) => {
     console.log({ err });
     process.exit(1);
   });
+
+export default server; 
